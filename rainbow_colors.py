b1e35dedc7632ebcbbe4d2f4ac92d43b92dabd5a
fling4169 <fling4169@gmail.com>
2025-05-27 05:47:21 -0400

    Delete rainbow_colors.py

--- rainbow_colors.py
+++
@@ -1,9 +0,0 @@
-# Optional utility file
-import discord
-import colorsys
-
-def generate_rainbow_colors(n=100):
-    return [
-        discord.Color.from_rgb(*[int(c * 255) for c in colorsys.hsv_to_rgb(i / n, 1, 1)])
-        for i in range(n)
-    ]
